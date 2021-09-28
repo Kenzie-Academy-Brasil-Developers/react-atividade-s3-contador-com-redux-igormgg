f475e76b848d417c078ac6873549b7b3574849c8
Igor <igormgg@hotmail.com>
2021-09-28 14:25:52 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./reset.css";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Contador com Redux</header>
+      <main></main>
+    </div>
+  );
+}
+
+export default App;
